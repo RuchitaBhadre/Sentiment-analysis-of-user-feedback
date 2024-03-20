8057ed551012e3c344d52eb180ca136bd012f8a0
Ruchita Rajkumar Bhadre <ruchitabhadre@gmail.com>
2024-03-20 19:07:27 -0400

    Refactored app code for enhanced navigation

--- app.py
+++ app.py
@@ -2,11 +2,7 @@ from flask import Flask, render_template, request
 from forms import FeedbackForm
 import openai
 
-
-import requests
 import os 
-import uuid 
-import json 
 from dotenv import load_dotenv 
 load_dotenv() 
 
@@ -20,14 +16,9 @@ openai.api_key = 'YOUR_API_KEY'
 
 
 @app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template('homepage.html')
-
-@app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     form=FeedbackForm()
     return render_template('feedback.html', form=form)
-
 
 @app.route('/feedback_post', methods=['POST'])
 def feedback_post():
@@ -35,7 +26,6 @@ def feedback_post():
     # Load the values from .env 
     language_key = os.environ.get('KEY')
     language_endpoint = os.environ.get('ENDPOINT')
-    location = os.environ['LOCATION'] 
 
     # Authenticate the client using your key and endpoint 
     def authenticate_client():
@@ -48,7 +38,7 @@ def feedback_post():
     #create and authenticate the client
     client = authenticate_client()
     
-    #feedback received is in string and inout to text analytics has to be a document so convert it to one as follows:
+    #feedback received is in string and input to text analytics has to be a document so convert it to one as follows:
     documents=[
         {"id":"1","language":"en", "text":feedb}
     ]
@@ -56,17 +46,18 @@ def feedback_post():
     #Analyze the sentiment
     result= client.analyze_sentiment(documents, show_opinion_mining=True)
     senti=[r for r in result if not r.is_error]
-    print("Let's visualize the sentiment of each of these documents")
+    print("Let's visualize the sentiment of the feedback")
     for idx, doc in enumerate(senti):
         overall_sentiment=doc.sentiment
-        print(f"Document text: {documents[idx]}")
+        print(f"Document Text: {documents[idx]}")
         print(f"Overall sentiment: {doc.sentiment}")
     
+    #Preparing input to the LLM model
     llm_input=overall_sentiment + "," + feedb
     completion = openai.chat.completions.create(
         model="gpt-4",
         messages=[
-           {"role": "system", "content": "You are a movie recommender and receiving analyzed sentiment from Azure along with the original feedback from users. Provide appropriate and customised response to the feedback. The response should be very polite and try to retain its customers by providing responses accordingly. For example, if it is negative, apologize and ask if they would like another recommendation. If it is positive, express your appreciation for the feedback and ask if they want more suggestions. If it is neutral then suggest other options."},
+           {"role": "system", "content": "You are a movie recommender and receiving analyzed sentiment from Azure along with the original feedback from users. Provide appropriate and customised response to the feedback. The response should be very polite and try to retain its customers by providing responses accordingly. It is mandatory to keep the response short and straightforward. For example, if it is negative, apologize and ask if they would like another recommendation. If it is positive, express your appreciation for the feedback and ask if they want more suggestions. If it is neutral then suggest other options."},
            {"role": "user", "content": llm_input},
         ]
        )
